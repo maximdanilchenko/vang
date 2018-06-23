7185a90f24fda4d5dd49f2a53dab9543c620ad0a
Maxim Danilchenko <dmax.dev@gmail>
2018-06-23 23:27:44 +0300

    nested fields

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
-# vanga
-##### developing
+# vanga | _developing_
 
 ```python
 from vanga import fields
 from vanga.schema import Schema
 
+class Address(Schema):
+    street = fields.String()
+    house = fields.Integer()
+
 class User(Schema):
     name = fields.String()
     age = fields.Integer()
+    address = fields.Nested(Address())
 
 user_schema = User()
-validated = user_schema.validate({'name': 'Bob', 'age': '32'})
+validated = user_schema.validate({
+    'name': 'Bob',
+    'age': '32',
+    'address': {'street': 'High',
+                'house': 43}
+})
 ```
--- vanga/fields.py
+++ vanga/fields.py
@@ -1,28 +1,47 @@
+from abc import abstractmethod
+
 from vanga.abc import FieldABC
 from vanga.exceptions import VangaError
 
 
-class Integer(FieldABC):
+class Field(FieldABC):
     def __init__(self):
-        super(Integer, self).__init__()
+        """ TODO: Implement arguments like "required", "allow_none" etc """
+        pass
+
+    def _func(self, value):
+        return value
 
     def validate(self, key, data):
         try:
-            return int(data[key])
+            return self._func(data[key])
         except ValueError:
             raise VangaError(f'incorrect format for {key} value')
         except KeyError:
             raise VangaError(f'missing "{key}" value')
 
 
-class String(FieldABC):
-    def __init__(self):
-        super(String, self).__init__()
+class Integer(Field):
+    _func = int
+
+
+class String(Field):
+    _func = str
+
+
+class Boolean(Field):
+    _func = bool
+
+
+class Nested(Field):
+    def __init__(self, schema):
+        self._schema = schema
+        super(Nested, self).__init__()
 
     def validate(self, key, data):
         try:
-            return str(data[key])
-        except ValueError:
+            return self._schema.validate(data[key])
+        except VangaError:
             raise VangaError(f'incorrect format for {key} value')
         except KeyError:
             raise VangaError(f'missing "{key}" value')
